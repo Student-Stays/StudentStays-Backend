a0de94cd6fbf1e339373ee3139fcde6e93646de6
Siddhanttimeline <siddhanttimeline9@gmail.com>
2025-07-16 00:57:30 +0530

    make random changes

--- src/main/java/com/management/studentstays/App/impl/EmailService.java
+++ src/main/java/com/management/studentstays/App/impl/EmailService.java
@@ -33,10 +33,16 @@ public class EmailService {
     EmailTemplate template =
         templateRepository
             .findByName(templateName)
+
+
             .orElseThrow(() -> new EmailTemplateNotFoundException(templateName));
 
     templateData.put("recipient", template.getRecipient());
-    templateData.put("subject", template.getSubject());
+
+
+    templateData.put("subject",
+
+            template.getSubject());
 
     String templateContent = loadTemplateContent(templateName);
 
